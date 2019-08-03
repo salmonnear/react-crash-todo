9b38aab4801e6905a04f9e34a661c4c53c92e50c
Samuel Mennear <samuelmennear@gmail.com>
2019-08-03 12:01:16 -0400

    added Todo.js and TodoItem.js components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Meet with boss',
+        completed: false
+      }
+    ]
+  };
+
+
+  render() {
+
+    /*console.log(this.state.todos)*/
+
+    return (
+      <div className="App">
+        <Todos todos = {this.state.todos} />  {/* passing in todos as prop */}
+      </div>  
+    )
+  }
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export class TodoItem extends Component {
+    render() {
+        return (
+            <div>
+                <h3>{ this.props.todo.title }</h3>
+            </div>
+        )
+    }
+}
+
+
+// PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+
+export default TodoItem
