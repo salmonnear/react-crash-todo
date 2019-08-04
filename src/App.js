c20cdfbffd1c9ea158a1f183e84fa9c42ce3bbb2
Samuel Mennear <samuelmennear@gmail.com>
2019-08-04 13:08:38 -0400

    add add todos

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React, {Component} from 'react';
 import './App.css';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
+import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner',
         completed: true
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meet with boss',
         completed: false
       }
@@ -40,6 +42,14 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
   }
 
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
 
   render() {
 
@@ -47,9 +57,12 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Header />
-        <Todos todos = {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>  {/* passing in todos as prop */}
-      </div>  
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo}/>
+          <Todos todos = {this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>  {/* passing in todos as prop */}
+        </div>  
+      </div>
     )
   }
 }
