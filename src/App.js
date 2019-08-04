30394907676f36d9a3f083add358849d070fdaf9
Samuel Mennear <samuelmennear@gmail.com>
2019-08-04 18:10:16 -0400

    fixed unique id's for todos

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from './components/pages/About';
 import Todos from './components/Todos';
 import Header from './components/layout/Header';
 import AddTodo from './components/AddTodo';
-// import uuid from 'uuid';
+import uuid from 'uuid';
 import axios from 'axios';
 
 class App extends Component {
@@ -15,35 +15,47 @@ class App extends Component {
 
   // send Get request to jpholder with axios
   componentDidMount() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    axios
+        .get('https://jsonplaceholder.typicode.com/todos?_limit=10')
         .then(res => this.setState({ todos: res.data }));
   }
 
   // Toggle Complete
   markComplete = (id) => {
-    this.setState({ todos: this.state.todos.map(todo => {
-      if(todo.id === id) {
-        todo.completed = !todo.completed;
-      }
-      return todo;
-    }) });  
+    this.setState({ 
+      todos: this.state.todos.map(todo => {
+          if(todo.id === id) {
+            todo.completed = !todo.completed;
+          }
+          return todo;
+        }) 
+      });  
   }
+
 
   // Delete Todo
   delTodo = (id) => {
-    axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-      .then(res => this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] }))
-    ;
-  }
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+      .then(res =>
+         this.setState({ 
+           todos: [...this.state.todos.filter(todo => todo.id !== id)]
+          })
+      );
+  };
+
 
   addTodo = (title) => {
-    axios.post('https://jsonplaceholder.typicode.com/todos', {
-      title,
-      completed: false,
-    })
-      .then(res => this.setState({ todos:
-         [...this.state.todos, res.data] }));
-    
+    axios
+        .post('https://jsonplaceholder.typicode.com/todos', {
+            title,
+            completed: false,
+          })
+          .then(res => { 
+            res.data.id = uuid.v4();
+            this.setState({ todos:[...this.state.todos, res.data] })
+          });
+        
   }
 
   render() {
